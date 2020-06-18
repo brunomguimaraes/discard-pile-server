8690c238bfa455e3e2a0088ba06705936f9da5b8
Bruno <mguimaraesbruno@gmail.com>
2020-06-18 09:48:12 -0300

    first concepts

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,39 @@
-import express from 'express';
+import express, { response } from 'express';
 
 const app = express();
 
+app.use(express.json());
+
+const users = [
+    'Xabaluria',
+    'Laruxa',
+    'Bruxonilda',
+    'Ryu'
+];
+
 app.get('/users', (request, response) => {
-    console.log('User List!');
+    const search = String(request.query.search);
+    const filteredUsers = search ? users.filter( user => user.includes(search)) : users;
 
-    // JSON
+    return response.json(filteredUsers);
+});
 
-    response.json([
-        'Xabaluria',
-        'Laruxa',
-        'Bruxonilda',
-        'Ryu'
-    ]);
+app.get('/users/:id', (request, response) => {
+    const id = Number(request.params.id);
+
+    const user = users[id];
+
+    return response.json(user);
+})
+
+app.post('/users', (request, response) => {
+    const data = request.body;
+    const user = {
+        name: data.name,
+        email: data.email,
+    }
+
+    return response.json(user);
 });
 
 app.listen(3333);
